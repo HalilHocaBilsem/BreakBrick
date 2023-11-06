7bff489482fa2002d797f02db9dcf4787e6cfb04
@HalilHocaBilsem <karaarslanhalilbilsem@gmail.com>
2023-11-06 20:30:24 +0300

    Proje dosyası ekle.

---
+++ BreakBrick/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BreakBrick
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zemin
+            // 
+            this.zemin.BackColor = System.Drawing.Color.Black;
+            this.zemin.Location = new System.Drawing.Point(-3, 549);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(996, 34);
+            this.zemin.TabIndex = 0;
+            this.zemin.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Green;
+            this.player.Location = new System.Drawing.Point(309, 393);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(270, 18);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.Location = new System.Drawing.Point(368, 275);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.ball);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(993, 432);
+            this.panel1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 577);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.zemin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BreakBrick/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakBrick
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> tuglalar=new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int satir = 0; satir < 5; satir++)
+            {
+                for (int sutun = 0; sutun < 20; sutun++)
+                {
+                    PictureBox p = new PictureBox() { BackColor = Color.Brown, BorderStyle = BorderStyle.FixedSingle, Width = 50, Height = 20, Top = satir * 20, Left = sutun * 50 };
+                    panel1.Controls.Add(p);
+                    tuglalar.Add(p);
+                }
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (player.Left >= 10)
+                {
+                    player.Left -= 10;
+                }
+
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (player.Right <= panel1.Width - 20)
+                {
+                    player.Left += 10;
+                }
+
+            }
+        }
+
+        int yatayHareketYon = 2;
+        int dikeyHareketYon = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (ball.Bounds.IntersectsWith(player.Bounds) && ball.Top<player.Top)
+            {
+                dikeyHareketYon= - 1;
+            }
+
+            foreach (var tugla in tuglalar)
+            {
+                if (ball.Bounds.IntersectsWith(tugla.Bounds))
+                {
+                    dikeyHareketYon = 1;
+                    tuglalar.Remove(tugla);
+                    panel1.Controls.Remove(tugla);
+                    break;
+                }
+            }
+
+
+
+            if (ball.Bottom >= panel1.Height)
+            {
+                //burada oyun biter
+                timer1.Stop();
+                dikeyHareketYon = -1;
+                MessageBox.Show("Oyun Bitti");
+
+            }
+            else if (ball.Right >= panel1.Width)
+            {
+                yatayHareketYon = -2;
+            }
+            else if (ball.Top <= 0)
+            {
+                dikeyHareketYon = 1;
+            }
+            else if (ball.Left <= 0)
+            {
+                yatayHareketYon = 2;
+            }
+
+
+
+
+            ball.Left += yatayHareketYon;
+            ball.Top += dikeyHareketYon;
+        }
+    }
+}
