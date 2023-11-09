c97edf8cd9e07791b1cfe8bad91b75ecd43f784b
@HalilHocaBilsem <karaarslanhalilbilsem@gmail.com>
2023-11-09 19:27:50 +0300

    Form 2 eklendi

---
+++ BreakBrick/Form2.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BreakBrick
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.ball);
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 512);
+            this.panel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Blue;
+            this.player.Location = new System.Drawing.Point(240, 458);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(230, 20);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.Location = new System.Drawing.Point(204, 324);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 25);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 580);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ BreakBrick/Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakBrick
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+
+
+            for (int dikeyTuglaSayisi = 0; dikeyTuglaSayisi < 4; dikeyTuglaSayisi++)
+            {
+                for (int yatayTuglaSayisi = 0; yatayTuglaSayisi < 20; yatayTuglaSayisi++)
+                {
+                    PictureBox brick = new PictureBox();
+                    brick.BackColor = Color.Brown;
+                    brick.Size = new Size(50, 20);
+                    brick.BorderStyle = BorderStyle.FixedSingle;
+                    brick.Location = new Point(yatayTuglaSayisi * 50, 20*dikeyTuglaSayisi);
+                    brick.Tag = "brick";
+                    panel1.Controls.Add(brick);
+                }
+            }         
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+          this.Text=player.Left.ToString();
+            if (e.KeyCode==Keys.Left && player.Left>=10)
+            {
+                player.Left -= 10;
+            }
+            else if(e.KeyCode==Keys.Right && player.Right<panel1.Width) 
+            {
+                player.Left += 10;
+            }
+        }
+
+        int ballDirectionX = 2;
+        int ballDirectionY =2;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //alt kenara değerse oyun biter
+            if (ball.Bottom>=panel1.Height)
+            {
+                timer1.Stop();
+                MessageBox.Show("Oyun Bitti!");
+            }
+
+            foreach (var control in panel1.Controls)
+            {
+                if (control is PictureBox p)
+                {
+                    if (p.Tag!=null && p.Tag.ToString()=="brick")
+                    {
+                        if (ball.Bounds.IntersectsWith(p.Bounds))
+                        {
+                            ballDirectionY *= -1;
+                            panel1.Controls.Remove(p);
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            if (ball.Bounds.IntersectsWith(player.Bounds)|| ball.Top <= 0)
+            {
+                ballDirectionY *= -1;
+            }
+            if (ball.Right>=panel1.Width|| ball.Left <= 0)
+            {
+                ballDirectionX *= -1;
+            }
+         
+            
+
+           ball.Location=new Point(ball.Left+ballDirectionX, ball.Top+ballDirectionY);
+        }
+    }
+}
